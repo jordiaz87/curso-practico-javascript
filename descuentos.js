a6033803c5c28b0da015c10181893a5de0a6c19e
Jorge Diaz <jorjorf@gmail.com>
2022-03-09 15:05:35 -0400

    se crea la funcion de l operacion y se observan los resultados en consola

--- descuentos.js
+++ descuentos.js
@@ -1,14 +1,20 @@
-const precioOriginal = 120;
-const descuento = 18;
-
-const porcentajePrecioConDescuento = 100 - descuento;
-
-const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100;
+// const precioOriginal = 120;
+// const descuento = 18;
 
 
-console.log({
-    precioOriginal,
-    descuento,
-    porcentajePrecioConDescuento,
-    precioConDescuento
-});
+function calcularPrecioConDescuento(precio, descuento){
+    
+    const porcentajePrecioConDescuento = 100 - descuento;
+
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+
+// console.log({
+//     precioOriginal,
+//     descuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento
+// });

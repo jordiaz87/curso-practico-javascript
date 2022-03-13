f2738812512706b33711a652fdba5c4771ab886c
Jorge Diaz <jorjorf@gmail.com>
2022-03-13 11:06:33 -0400

    Se crean archivos html , js de la mediana y funciones para calcularla con una lista ordenada

---
+++ mediana.js
@@ -0,0 +1,67 @@
+const lista1 = [
+    100,
+    200,
+    500,
+    
+    4000000000
+];
+
+const mitadLista1 = parseInt(lista1.length / 2);
+
+let mediana;
+
+
+// function esPar(numerito){
+//     if (numerito es par?){
+//         return true;
+//     }else {
+//         return false;
+//     }
+// }
+
+function esPar(numerito){
+    if (numerito % 2 === 0){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+// if (lista1 es par?){
+//     necesitamos dos elementos
+//     el promedio
+//     mediana
+// } else {
+//     posicion mitadLista1 dentro de lista1
+//     mediana
+// }
+
+if (esPar(lista1.length)){
+    const elemento1 = lista1[mitadLista1 - 1];
+    const elemento2 = lista1[mitadLista1];
+
+    const promedioElemento1y2 = calcularMediaAritmetica([
+        elemento1,
+        elemento2
+    ])
+
+    mediana = promedioElemento1y2;
+
+} else {
+    mediana = lista1[mitadLista1];
+}
+
+function calcularMediaAritmetica(lista) {
+    
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento ){
+            return valorAcumulado + nuevoElemento;
+        }
+    ); 
+
+    const promedioLista = sumaLista / lista.length;
+
+    return promedioLista;
+
+}
